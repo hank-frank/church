88d36d1d3c3ba8f8be9b0b111b40563f4250bc67
hankfrank <henryfrank1@yahoo.com>
2020-01-04 10:45:24 -0800

    inplements test calls for both Smartsheets and PC

--- server/server.js
+++ server/server.js
@@ -1,19 +1,112 @@
 const express = require('express');
 const axios = require('axios');
+const auth = require('basic-auth');
+const client = require('smartsheet');
+const dotenv = require('dotenv').config();
+// const pcBasicAuth = 'Basic' + btoa(process.env.PC_APPLICATION_ID + ":" + process.env.PC_SECRET)
 
 const app = express();
 
+var smartsheet = client.createClient({ accessToken: process.env.SMARTSHEET_BEARER_TOKEN });
+
+
 app.use(express.static('dist'));
 
-// app.get('/api', (req, res) => {
-//     axios.get(`http://www.mocky.io/v2/5d5cba7e320000a5e4628f33?apikey=${process.env.APIKEY}`)
-//         .then((result) => {
-//             res.send(result.data);
-//         })
-//         .catch((error) => {
-//             console.error(error);
-//             res.send('An error occured.');
-//         })
-// });
+
+app.get('/ss', (req, res) => {
+
+    var options = {
+        queryParameters: {
+        include: "attachments",
+        includeAll: true
+        }
+    };
+    
+    smartsheet.sheets.listSheets(options)
+        .then(function (result) {
+            // Choose the first sheet
+            let sheetId = result.data[0].id;
+
+            // Load one sheet
+            smartsheet.sheets.getSheet({id: sheetId})
+            .then(function(sheetInfo) {
+                console.log(sheetInfo);
+                // drill down to the sheets rows
+                let rows = sheetInfo.rows[0.].cells;
+                rows.forEach((each) => {
+                    //check for specific text in a row/col. 
+                    if (each.value == "col 2 text") {
+                        console.log("this shit worked!")
+                    }
+                })
+                console.log(`rows cell info: `, rows[0].cells)
+                res.send(sheetInfo);
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+});
+
+app.get('/pc-people', (req, res) => {
+    axios({
+        method: 'get', 
+        url: 'https://api.planningcenteronline.com/people/v2/people?where[henry]=string',
+        auth: {
+            username: process.env.PC_APPLICATION_ID,
+            password: process.env.PC_SECRET
+        }
+    })
+    .then((response) => {
+        console.log(`data.attributes: `, response.data.data);
+        res.send(response.data);
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+// app.get('/pc-event', (req, res) => {
+//     axios({
+//         method: 'get', 
+//         url: 'https://api.planningcenteronline.com//webhooks/v2/subscriptions',
+//         auth: {
+//             username: process.env.PC_APPLICATION_ID,
+//             password: process.env.PC_SECRET
+//         }
+//     })
+//     .then((response) => {
+//         console.log(response.data);
+//         console.log(`attribures: `, response.data.data[0].attributes);
+//         console.log(`links: `, response.data.data[0].links);
+//         res.send(response.data);
+//     })
+//     .catch((err) => {
+//         console.log(err)
+//     })
+// })
+
+app.get('/pc-event', (req, res) => {
+    axios({
+        method: 'get', 
+        url: 'https://api.planningcenteronline.com/webhooks/v2/available_events',
+        auth: {
+            username: process.env.PC_APPLICATION_ID,
+            password: process.env.PC_SECRET
+        }
+    })
+    .then((response) => {
+        console.log(` subscription deliveries: `, response.data);
+        
+        res.send(response.data);
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
 
 module.exports = app;
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,8 +3,47 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 import { render } from 'react-dom';
 
 function App () {
+
+    const smartsheetTest = () => {
+        fetch('/ss')
+            .then((response) => {
+                let reader = response.body.getReader();
+                console.log(`SS reader: `, reader)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+
+    }
+
+    const PCPeople = () => {
+        fetch('/pc-people')
+            .then((response) => {
+                let reader = response.body.getReader();
+                console.log(`PC reader: `, reader)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    const PCEvent = () => {
+        fetch('/pc-event')
+            .then((response) => {
+                let reader = response.body.getReader();
+                console.log(`PC reader: `, reader)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
     return (
-        <h1>Testing this hot dirty mother!</h1>
+        <>
+            <button onClick={ smartsheetTest }>Smartsheet test!</button>
+            <button onClick={ PCPeople }>planningCenter people test</button>
+            <button onClick={ PCEvent }>planningCenter event test</button>
+        </>
     )
 };
 
